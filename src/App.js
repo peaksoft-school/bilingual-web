70ce5f83065bec4b80e7fdc007f56f4b1703b833
Almazbek uulu Adilet <aduletalmaz@gmail.com>
2022-01-26 13:11:50 +0300

    ReusableInput

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css'
+import classes from './App.module.css'
+import Input from './components/UI/input/Input'
 
 function App() {
-   return <div>hi</div>
+   return (
+      <div className={classes.combox}>
+         <Input />
+      </div>
+   )
 }
 
 export default App
