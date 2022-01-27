fb9075ebc41f37c3777feb2b015f218348591855
bolotbekMM <bolotbek.muratov@gmail.com>
2022-01-27 18:04:49 +0600

    just commented

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import { useState } from 'react'
-import './App.css'
-import ReusableSwitch from './components/UI/switchButton'
-
 function App() {
-   const [value, setValue] = useState(false)
-   const [on, setOn] = useState(false)
-
-   const changeHandlerFirst = () => {
-      setValue(!value)
-   }
-
-   const changeHandlerSecond = () => {
-      setOn(!on)
-   }
-
-   const submitHandler = (e) => {
-      e.preventDefault(e)
-   }
-
-   return (
-      <form onSubmit={submitHandler}>
-         <div>
-            <ReusableSwitch checked={value} onChange={changeHandlerFirst} />
-         </div>
-         <div>
-            <ReusableSwitch checked={on} onChange={changeHandlerSecond} />
-         </div>
-      </form>
-   )
+   return <div>hi</div>
 }
 
 export default App
