921d91545d1c12b8f6a2cd710b995536c2bc398c
bolotbekMM <bolotbek.muratov@gmail.com>
2022-01-28 17:16:41 +0600

    module css changed to styled component

--- src/components/UI/switchButton/index.js
+++ src/components/UI/switchButton/index.js
@@ -1,9 +1,56 @@
-import classes from './Switch.module.css'
+import styled from 'styled-components'
 
-export default function ReusableSwitch({ onChange, checked }) {
+const Label = styled.label`
+   display: flex;
+   align-items: center;
+   gap: 10px;
+   cursor: pointer;
+`
+
+const Switch = styled.div`
+   position: relative;
+   width: 39px;
+   height: 22px;
+   background: #b3b3b3;
+   border-radius: 20px;
+   padding: 4px;
+   transition: 300ms all;
+
+   &:before {
+      transition: 300ms all;
+      content: '';
+      position: absolute;
+      width: 22px;
+      height: 22.6px;
+      border-radius: 20px;
+      top: 4.5px;
+      left: 1.5px;
+      background: white;
+      transform: translate(1px, -1.2px);
+      transition: 0.3s;
+   }
+`
+
+const Input = styled.input`
+   opacity: 1;
+   position: absolute;
+
+   &:checked + ${Switch} {
+      background: #2ab930;
+
+      &:before {
+         transform: translate(21px, -1.2px);
+      }
+   }
+`
+
+const SwitchButton = ({ onChange, checked }) => {
    return (
-      <div className={classes.center}>
-         <input type="checkbox" name="" onChange={onChange} checked={checked} />
-      </div>
+      <Label>
+         <Input type="checkbox" checked={checked} onChange={onChange} />
+         <Switch />
+      </Label>
    )
 }
+
+export default SwitchButton
