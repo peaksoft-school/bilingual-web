530f8ba770c457ad4594a8f92daeaebd5cc8e13d
bolotbekMM <bolotbek.muratov@gmail.com>
2022-02-01 23:57:38 +0600

    extra div removed

--- src/components/UI/checkbox/index.js
+++ src/components/UI/checkbox/index.js
@@ -3,17 +3,15 @@ import Checkbox from '@mui/material/Checkbox'
 
 export default function ReCheckbox({ checked, onChange, ...otherProps }) {
    return (
-      <div>
-         <Checkbox
-            checked={checked}
-            onChange={onChange}
-            {...otherProps}
-            sx={{
-               '&.Mui-checked': {
-                  color: '#2AB930',
-               },
-            }}
-         />
-      </div>
+      <Checkbox
+         checked={checked}
+         onChange={onChange}
+         {...otherProps}
+         sx={{
+            '&.Mui-checked': {
+               color: '#2AB930',
+            },
+         }}
+      />
    )
 }
