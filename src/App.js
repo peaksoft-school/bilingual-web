85cfe2483a8779d18a0d6afd7555cf01a5347349
bolotbekMM <bolotbek.muratov@gmail.com>
2022-02-01 18:46:51 +0600

    finished checkbox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import { useState } from 'react'
-import Checkbox from './components/UI/checkbox'
-
 function App() {
-   const [valu, setCheckbo] = useState(false)
-   return (
-      <div>
-         <Checkbox
-            label="1 вопрос"
-            value={valu}
-            checked={valu}
-            onChange={() => setCheckbo(!valu)}
-         />
-      </div>
-   )
+   return <div></div>
 }
 
 export default App
--- src/components/UI/checkbox/index.js
+++ src/components/UI/checkbox/index.js
@@ -1,96 +1,19 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import Checkbox from '@mui/material/Checkbox'
 
-const Input = styled.input`
-   height: 0;
-   width: 0;
-   opacity: 0;
-   z-index: -1;
-`
-
-const Label = styled.label`
-   position: relative;
-   display: inline-block;
-   cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
-   margin: 0.6em 1em;
-`
-
-const rotate = keyframes`
- from {
-    opacity: 0;
-    transform: rotate(0deg);
-  }
-  to {
-    opacity: 1;
-    transform: rotate(50deg);
-  }
-`
-
-const Indicator = styled.div`
-   width: 18.49px;
-   height: 18.49px;
-   position: absolute;
-   top: 0em;
-   left: 0em;
-   border: 2px solid #9a9a9a;
-   border-radius: 4px;
-
-   ${Input}:not(:disabled):checked & {
-      background: black;
-   }
-
-   ${Label}:hover & {
-      background: none;
-   }
-
-   &::after {
-      content: '';
-      position: absolute;
-      display: none;
-      background-color: black;
-   }
-
-   ${Input}:checked + &::after {
-      display: block;
-      top: 0.1em;
-      left: 0.35em;
-      width: 30%;
-      height: 45%;
-      border: solid #ffffff;
-      border-width: 0 0.2em 0.2em 0;
-      background: black;
-      animation-name: ${rotate};
-      animation-duration: 0.1s;
-      animation-fill-mode: forwards;
-   }
-
-   &::disabled {
-      cursor: not-allowed;
-   }
-`
-
-export default function Checkbox({
-   value,
-   checked,
-   onChange,
-   name,
-   id,
-   label,
-   disabled,
-}) {
+export default function ReCheckbox({ checked, onChange, ...otherProps }) {
    return (
-      <Label htmlFor={id} disabled={disabled}>
-         {label}
-         <Input
-            id={id}
-            type="checkbox"
-            name={name}
-            value={value}
-            disabled={disabled}
+      <div>
+         <Checkbox
             checked={checked}
             onChange={onChange}
+            {...otherProps}
+            sx={{
+               '&.Mui-checked': {
+                  color: '#2AB930',
+               },
+            }}
          />
-         <Indicator />
-      </Label>
+      </div>
    )
 }
