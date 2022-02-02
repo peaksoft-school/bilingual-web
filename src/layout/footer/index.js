bd5b635831510d15500372e58df49733a901081f
bolotbekMM <bolotbek.muratov@gmail.com>
2022-02-02 13:22:47 +0600

    design of footer was changed

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -3,53 +3,52 @@ import styled from 'styled-components'
 import Logo from '../../assets/icons/logo.svg'
 
 const StyledFooterDiv = styled.div`
-   position: absolute;
    bottom: 0;
-   margin-bottom: 0px;
    width: 100%;
-   height: 109px;
+   height: 95px;
    background-color: white;
    box-sizing: border-box;
-`
-const StyledMainDiv = styled.div`
    margin-bottom: 0px;
+   margin-top: 60px;
+`
+const StyledDiv = styled.div`
+   width: 1140px;
    margin-left: auto;
    margin-right: auto;
-   padding: 62px 153px 14.98px 153px;
-   max-width: 1440px;
-   max-height: 109px;
+`
+const StyledMainDiv = styled.div`
+   width: 100%;
+   height: 95px;
    display: flex;
-   justify-content: space-between;
+   justify-content: center;
    align-items: center;
    box-sizing: border-box;
 `
 const StyledLogo = styled.img`
-   margin: 0;
-   width: 125px;
-   height: 44px;
-`
-const StyledCopyrightDiv = styled.div`
-   padding: 4px 0px;
+   margin-right: 6px;
+   width: 135px;
+   height: 54px;
 `
 
 const StyledCopyright = styled.span`
    font-family: 'Inter';
    font-style: normal;
    font-weight: normal;
-   font-size: 14px;
-   line-height: 24px;
+   padding-top: 3px;
+   font-size: 15px;
+   margin-left: 6px;
    color: #98a2b3;
 `
 
 const Footer = () => {
    return (
       <StyledFooterDiv>
-         <StyledMainDiv>
-            <StyledLogo src={Logo} alt="Logo" />
-            <StyledCopyrightDiv>
+         <StyledDiv>
+            <StyledMainDiv>
+               <StyledLogo src={Logo} alt="Logo" />
                <StyledCopyright>© 2022 Peaksoft</StyledCopyright>
-            </StyledCopyrightDiv>
-         </StyledMainDiv>
+            </StyledMainDiv>
+         </StyledDiv>
       </StyledFooterDiv>
    )
 }
