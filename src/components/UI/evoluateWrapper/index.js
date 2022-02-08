5322190bef0c143db019b83643d2c3f377932eb5
kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2022-02-07 19:51:29 -1000

    save

--- src/components/UI/evoluateWrapper/index.js
+++ src/components/UI/evoluateWrapper/index.js
@@ -1,86 +1,133 @@
 import * as React from 'react'
 import Table from '@mui/material/Table'
-import { Paper, Checkbox } from '@mui/material'
-import TableBody from '@mui/material/TableBody'
+import styled from 'styled-components'
+import { Paper } from '@mui/material'
+import { DeleteOutlined } from '@mui/icons-material'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
+import Header from '../../../layout/adminHeader'
+import MainContainer from '../../../layout/MainContainer'
+import ReCheckbox from '../checkbox'
 
-function createData(userName, time, testNumber, evoluate, icon) {
-   return { userName, time, testNumber, evoluate, icon }
+function createData(number, userName, time, testNumber, evoluate, icon) {
+   return { number, userName, time, testNumber, evoluate, icon }
 }
 
 const rows = [
-   createData('1', 'ANNA', '08:15', 'Test number 1', 'Not evaluated'),
-   createData('1', 'ANNA', '08:15', 'Test number 1', 'Not evaluated'),
+   createData('1', 'Adilet', '08:15', 'Test number 1', 'Not evaluated'),
+   createData('2', 'Nurs', '08:15', 'Test number 1', 'Not evaluated'),
    createData('3'),
    createData('4'),
 ]
 
-export default function BasicTable({ rows }) {
+export default function BasicTable() {
    return (
-      <TableContainer
-         component={Paper}
-         sx={{
-            borderRadius: '20px',
-            boxShadow: '0px 4px 39px rgba(196, 196, 196, 0.6)',
-            padding: '50px 115px',
-            width: '70%',
-            height: '80%',
-            mx: 'auto',
-            marginTop: '40px',
-         }}
-      >
-         <Table sx={{ tableLayout: 'auto' }} aria-label="simple table">
-            <TableHead>
-               <TableRow>
-                  <TableCell>#</TableCell>
-                  <TableCell align="center">User Name</TableCell>
-                  <TableCell align="center">Date of Submitiion</TableCell>
-                  <TableCell align="center">Question Type</TableCell>
-                  <TableCell align="center">Status</TableCell>
-                  <TableCell>.</TableCell>
-               </TableRow>
-            </TableHead>
-            <TableBody>
-               {rows.map((row) => (
-                  <TableRow
-                     key={row.name}
-                     sx={{
-                        height: '66px',
-                        color: '#4C4859',
-                     }}
-                  >
-                     <TableCell
-                        component="th"
-                        scope="row"
-                        sx={{ color: '#4C4859', padding: '0px 0px' }}
-                        padding="0px 0px"
-                     >
-                        {row.userName}
-                     </TableCell>
-                     <TableCell
-                        sx={{ color: '#4C4859', fontSize: '16px' }}
-                        align="center"
-                     >
-                        {row.time}
-                     </TableCell>
-                     <TableCell
-                        sx={{ color: '#4C4859', fontSize: '16px' }}
-                        align="center"
-                     >
-                        {row.testNumber}
-                     </TableCell>
-                     <TableCell align="center">{row.evoluate}</TableCell>
-                     <TableCell align="center">{row.evoluate}</TableCell>
-
-                     <Checkbox />
-                     <Checkbox />
-                  </TableRow>
-               ))}
-            </TableBody>
-         </Table>
-      </TableContainer>
+      <>
+         <Header />
+         <MainContainer>
+            <TableContainer component={StyledPaper}>
+               <Table
+                  sx={{ borderCollapse: 'separate', borderSpacing: '10px' }}
+               >
+                  <TableHead>
+                     <TableRow sx={{ color: '#4C4859' }}>
+                        <StyledHead>#</StyledHead>
+                        <StyledHead>User Name</StyledHead>
+                        <StyledHead align="center">
+                           Date of Submitiion
+                        </StyledHead>
+                        <StyledHead align="center">Question Type</StyledHead>
+                        <StyledHead align="center">Status</StyledHead>
+                        {/* <StyledHead>.</StyledHead> */}
+                     </TableRow>
+                  </TableHead>
+                  {rows.map((row) => (
+                     <StyletTableRow key={row.number} align="center">
+                        <StyledTableCell align="center">
+                           {row.number}
+                        </StyledTableCell>
+                        <StyledTableCell align="center">
+                           {row.userName}
+                        </StyledTableCell>
+                        <StyledTableCell align="center">
+                           {row.time}
+                        </StyledTableCell>
+                        <StyledTableCell>{row.testNumber}</StyledTableCell>
+                        <StyledTableCell align="center">
+                           {row.evoluate}
+                        </StyledTableCell>
+                        <StyledTableCell align="center">
+                           <Div>
+                              <ReCheckbox />
+                              <DeleteIcon />
+                           </Div>
+                        </StyledTableCell>
+                     </StyletTableRow>
+                  ))}
+               </Table>
+            </TableContainer>
+         </MainContainer>
+      </>
    )
 }
+export const StyletTableRow = styled(TableRow)`
+   &.MuiTableRow-root {
+      border-radius: 8px;
+      background: #ffffff;
+      /* border-bottom: none !important; */
+      box-shadow: 0px 3px 14px rgba(0, 0, 0, 0.09);
+      padding: 30px;
+      align-content: space-between;
+      border-collapse: separate;
+      border-spacing: 0 5em;
+      padding-top: 15px;
+   }
+`
+
+export const StyledTableCell = styled(TableCell)`
+   &.MuiTableCell-body {
+      border-radius: 8px;
+      color: '#4C4859';
+      font-size: 16px;
+      font-family: 'DINNextRoundedLTW01-Regular';
+      font-style: normal;
+   }
+`
+const DeleteIcon = styled(DeleteOutlined)`
+   &.MuiSvgIcon-root {
+      margin-top: 9px;
+      margin-left: 20px;
+   }
+`
+const Div = styled.div`
+   display: flex;
+   justify-content: column;
+`
+export const StyledHead = styled(TableCell)`
+   &.MuiTableCell-head {
+      border-bottom: 'none';
+      border-collapse: separate;
+      background: transparent;
+      border: none;
+      color: #4c4859;
+      font-family: 'DINNextRoundedLTW04-Medium';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 16px;
+   }
+`
+export const StyledPaper = styled(Paper)`
+&.MuiPaper-root{
+   border-radius: 20px;
+   box-shadow: 0px 4px 39px rgba(196, 196, 196, 0.6);
+   padding: 50px 115px ;
+   width: 80%;
+   height: 90%,
+   margin-left: auto;
+   margin-top: 66px;
+   border-collapse: separate;
+   border-spacing: 0 5em;
+}
+`
