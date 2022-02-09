b7dde9c1d84dc24fb8dd39953aac35f102917eec
sakydin <sakydin.tashmurzaev.kk@gmail.com>
2022-02-09 19:45:44 +0600

    added axiosInstance

--- src/api/authService.js
+++ src/api/authService.js
@@ -1,15 +1,11 @@
-import axios from 'axios'
-
-const instance = axios.create({
-   baseURL: 'http://3.65.208.103/api/',
-})
+import { axiosInstance } from './axiosInstance'
 
 const signUpRequest = (signUpData) => {
-   return instance.post(`user/signup`, signUpData)
+   return axiosInstance.post(`user/signup`, signUpData)
 }
 
 const loginRequest = (signUpData) => {
-   return instance.post(`user/login`, signUpData)
+   return axiosInstance.post(`user/login`, signUpData)
 }
 
 const authService = { signUpRequest, loginRequest }
