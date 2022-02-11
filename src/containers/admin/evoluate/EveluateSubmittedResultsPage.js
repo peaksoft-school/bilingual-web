66d13765673dc42c12292424cbeb249ea4d2e1aa
kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2022-02-11 13:14:35 -1000

    start

---
+++ src/containers/admin/evoluate/EveluateSubmittedResultsPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../../layout/adminHeader/index'
+import MainContainer from '../../../layout/MainContainer'
+
+function EveluateSubmittedResultsPage() {
+   return (
+      <>
+         <Header />
+         <MainContainer />
+      </>
+   )
+}
+
+export default EveluateSubmittedResultsPage
