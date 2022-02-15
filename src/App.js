35b9c1be9edef2c253d4b2b0341dda0c42fc0cf8
kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2022-02-15 13:13:00 -1000

    to pr

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { authActions } from './store'
 import { getFromLocalStorage } from './utils/helpers/localstorege/localStorege'
 import { theme } from './assets/styles/themeStyle/theme'
 import { BILINGUAL_TOKEN, BILINGUAL_USER } from './utils/constants/general'
+import DescribeImagePage from './containers/admin/evaluate/describeImagePage/DescribeImagePage'
 
 function App() {
    const dispatch = useDispatch()
@@ -30,7 +31,8 @@ function App() {
 
    return (
       <ThemeProvider theme={theme}>
-         <Routes />
+         {/* <Routes /> */}
+         <DescribeImagePage />
       </ThemeProvider>
    )
 }
