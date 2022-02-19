f4ee6ca00c532d66d395c18d3b53f8ea5d0d7d76
sakydin <sakydin.tashmurzaev.kk@gmail.com>
2022-02-19 18:05:03 +0600

    added a private route and interceptor

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,24 @@
 import axios from 'axios'
+import { BILINGUAL_TOKEN } from '../utils/constants/general'
+import { getFromLocalStorage } from '../utils/helpers/localstorege/localStorege'
+
+const token = getFromLocalStorage(BILINGUAL_TOKEN)
+
+const headers = {
+   Authorization: `Bearer ${token}`,
+}
 
 export const axiosInstance = axios.create({
-   baseURL: 'http://3.65.208.103/api/',
+   baseURL: 'http://3.65.208.103/',
 })
+
+axiosInstance.interceptors.request.use(
+   async (config) => {
+      // eslint-disable-next-line no-param-reassign
+      config.headers = headers
+      return config
+   },
+   (error) => {
+      Promise.reject(error)
+   }
+)
