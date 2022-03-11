651a942ecf219b72c7611b0f47280a268e6200ac
sakydin <sakydin.tashmurzaev.kk@gmail.com>
2022-03-11 18:57:05 +0600

    select words;

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { getFromLocalStorage } from './utils/helpers/localstorege/localStorege'
 import { theme } from './assets/styles/themeStyle/theme'
 import { BILINGUAL_TOKEN, BILINGUAL_USER } from './utils/constants/general'
 
-import Routes from './routes/Routes'
+// import Routes from './routes/Routes'
+import UserSelectRealEnglishWords from './containers/client/userSelectRealEnglishWords/UserSelectRealEnglishWords'
 
 function App() {
    const dispatch = useDispatch()
@@ -31,7 +32,8 @@ function App() {
 
    return (
       <ThemeProvider theme={theme}>
-         <Routes />
+         {/* <Routes /> */}
+         <UserSelectRealEnglishWords />
       </ThemeProvider>
    )
 }
