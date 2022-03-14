33a909553b2d0690f883c79347f789a3199874f7
sakydin <sakydin.tashmurzaev.kk@gmail.com>
2022-03-14 11:11:15 +0600

    added disabled

--- src/containers/admin/tests/testPage/TestItem.js
+++ src/containers/admin/tests/testPage/TestItem.js
@@ -13,7 +13,6 @@ const TestItem = ({ onClickToDelete, id, active, toggleHandler }) => {
       setAccessToTest((accessToTest) => !accessToTest)
       toggleHandler({ id, isActive: !accessToTest })
    }
-   console.log(id, 'admin id')
 
    const [confirmDeleteModal, setConfirmDeleteModal] = React.useState(false)
    const modalHandler = () => {
